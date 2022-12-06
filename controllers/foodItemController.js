7c837f844bb99d312932319458108805bf18640f
Shashank Talwar <shashanktalwar81@gmail.com>
2022-12-06 12:10:58 +0530

    update food item using form

--- controllers/foodItemController.js
+++ controllers/foodItemController.js
@@ -1,5 +1,6 @@
 const FoodCategory = require('../models/Category.js');
 const FoodItem = require('../models/FoodItem.js');
+const async = require('async');
 
 exports.getFoodItemById = (req, res, next) => {
     // get foodItems with foodItem id as specified in req.params.id
@@ -53,7 +54,7 @@ exports.createNewFoodItem = (req, res) => {
             if(err) return next(err);
             //render FoodItemForm page
             res.render('foodItemForm',{
-                foodItem:null,
+                foodItem:foodItem,
                 categories,
                 selected:req.body.category
             });
@@ -65,17 +66,67 @@ exports.createNewFoodItem = (req, res) => {
         if(err) return next(err);
         //redirect to /categories/category/id route
         res.redirect(foodItem.url);
-    })
+    });
 }
 
 exports.getUpdateFoodItemForm = (req, res) => {
     //get FoodItem by id as specified in req.param.id
-    //render FoodItemForm page and populate fields with data found in foodItem
-}
+    async.parallel({
+        foodItem(callback){
+            FoodItem.findById(req.params.foodId, callback);
+        },
+        categories(callback){
+            FoodCategory.find({}, 'name _id',callback);
+        }
+    },
+    (err, results) => {
+        if(err) return next(err);
+        if(results.foodItem === null){
+            const error = new Error('Food Item not found');
+            error.status = 404;
+            return next(error);
+        }
+        //render FoodItemForm page and populate fields with data found in foodItem
+        res.render('foodItemForm',{
+            foodItem: results.foodItem,
+            categories: results.categories,
+            selected:results.foodItem.category
+        });
+    });
+};
 
 exports.updateFoodItem = (req, res) => {
-    //update foodItem object and save it to database
-    //redirect to /foodItems/foodItem/id route
+    //create new foodItem object
+    const foodItem = new FoodItem({
+        name: req.body.name,
+        description: req.body.descp ,
+        category: req.body.category,
+        price: req.body.price,
+        stock: req.body.stock,
+        _id: req.params.foodId,
+    })
+    // check for errors
+    const error = foodItem.validateSync();
+    if(error){
+        // if error, render form with error messages
+        console.log(error.errors);
+        FoodCategory.find({},'name _id',function(err, categories){
+            if(err) return next(err);
+            //render FoodItemForm page
+            res.render('foodItemForm',{
+                foodItem:foodItem,
+                categories,
+                selected:req.body.category
+            });
+        });
+        return;
+    }
+    // if no error then save to database
+    FoodItem.findByIdAndUpdate(req.params.foodId, foodItem, {}, (err, updatedFoodItem) => {
+        if(err) return next(err);
+        //redirect to /foodItems/foodItem/id route
+        res.redirect(updatedFoodItem.url);
+    })
 }
 
 exports.getDeleteFoodItemForm = (req, res) => {
